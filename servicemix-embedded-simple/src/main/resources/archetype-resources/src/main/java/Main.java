d262392b698d71f2f33e75a7c164af8eecd432b7
Guillaume Nodet <gnodet@apache.org>
2007-11-26 10:42:20 +0000

    Fix embedded archetype
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/trunk/archetypes@598213 13f79535-47bb-0310-9956-ffa450edef68

--- servicemix-embedded-simple/src/main/resources/archetype-resources/src/main/java/Main.java
+++ servicemix-embedded-simple/src/main/resources/archetype-resources/src/main/java/Main.java
@@ -29,22 +29,21 @@ public class Main {
 	public static void main(String[] args) {
 		// This is a very simple example of how you might embed ServiceMix
 		try {
-			ApplicationContext context = null;
+			final ApplicationContext context = new ClassPathXmlApplicationContext("servicemix.xml");
 
-			context = new ClassPathXmlApplicationContext("servicemix.xml");
-
-			SpringJBIContainer container = (SpringJBIContainer) context
-					.getBean("jbi");
-			Object lock = new Object();
-			container.setShutdownLock(lock);
-
-			// lets wait until we're killed.
-			synchronized (lock) {
-				lock.wait();
-			}
-			if (context instanceof DisposableBean) {
-				((DisposableBean) context).destroy();
-			}
+			SpringJBIContainer container = (SpringJBIContainer) context.getBean("jbi");
+            container.onShutDown(new Runnable() {
+                public void run() {
+                    if (context instanceof DisposableBean) {
+                        try {
+                            ((DisposableBean) context).destroy();
+                        } catch (Exception e) {
+                            System.out.println("Caught: " + e);
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
 		} catch (Exception e) {
 			System.out.println("Caught: " + e);
 			e.printStackTrace();
