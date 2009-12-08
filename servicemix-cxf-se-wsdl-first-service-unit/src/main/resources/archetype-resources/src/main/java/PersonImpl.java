3dc7555afb01468e8d4f43e82dfbb786707eea33
Freeman Yue Fang <ffang@apache.org>
2009-12-08 04:34:13 +0000

    [SM-1917]servicemix-cxf-se-wsdl-first-service-unit generates a wrong package structure
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/archetypes/trunk@888254 13f79535-47bb-0310-9956-ffa450edef68

--- servicemix-cxf-se-wsdl-first-service-unit/src/main/resources/archetype-resources/src/main/java/PersonImpl.java
+++ servicemix-cxf-se-wsdl-first-service-unit/src/main/resources/archetype-resources/src/main/java/PersonImpl.java
@@ -14,15 +14,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.servicemix.samples.wsdl_first;
+package ${packageName};
 
 import javax.jws.WebService;
 import javax.xml.ws.Holder;
-
+import org.apache.servicemix.samples.wsdl_first.Person;
+import org.apache.servicemix.samples.wsdl_first.UnknownPersonFault;
 import org.apache.servicemix.samples.wsdl_first.types.GetPerson;
 import org.apache.servicemix.samples.wsdl_first.types.GetPersonResponse;
 
 @WebService(serviceName = "PersonService", targetNamespace = "http://servicemix.apache.org/samples/wsdl-first", endpointInterface = "org.apache.servicemix.samples.wsdl_first.Person")
+
 public class PersonImpl implements Person {
 
     public void getPerson(Holder<String> personId, Holder<String> ssn, Holder<String> name)
@@ -31,7 +33,7 @@ public class PersonImpl implements Person {
         if (personId.value == null || personId.value.length() == 0) {
             org.apache.servicemix.samples.wsdl_first.types.UnknownPersonFault fault = new org.apache.servicemix.samples.wsdl_first.types.UnknownPersonFault();
             fault.setPersonId(personId.value);
-            throw new UnknownPersonFault(null, fault);
+            throw new UnknownPersonFault(null,fault);
         }
         name.value = "Guillaume";
         ssn.value = "000-000-0000";
